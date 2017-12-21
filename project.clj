64f3e77c11ce63837a837f22dfda8d78b0669c18
Matthew Darling <matthew.darling@kirasystems.com>
2017-12-21 18:39:33 -0500

    Add minimum stuff to reproduce issue

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject cljfmt-defrecord-formatting-example "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cljfmt "0.5.7"]]
+  :cljfmt {:indents {defcomponent [[:block 1] [:inner 1]]
+                     defrecord [[:block 1] [:inner 1]]}})
---
+++ src/core.clj
@@ -0,0 +1,20 @@
+(ns core)
+
+(defrecord Complicated
+           [attr1 attr2 attr3 attr4
+            attr5 attr6 attr7 attr8]
+  component/Lifecycle
+  (start [component]
+    component)
+  (stop [component]
+    component))
+
+(defcomponent empty-component
+  [_ owner _]
+  (:mixins [])
+
+  (init-state [_]
+    {})
+
+  (render-state [_ state]
+    (html [:div])))
