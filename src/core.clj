32bae5215556c76d506c613b3734be405f344cf9
Matthew Darling <matthew.darling@kirasystems.com>
2017-12-22 22:21:59 -0500

    Fix typo to actually prove namespaced symbols work

--- src/core.clj
+++ src/core.clj
@@ -1,9 +1,9 @@
 (ns core
-  (:require [schema.cofe :as s]))
+  (:require [schema.core :as s]))
 
 (s/defrecord Complicated
-             [attr1 attr2 attr3 attr4
-              attr5 attr6 attr7 attr8]
+  [attr1 attr2 attr3 attr4
+   attr5 attr6 attr7 attr8]
   component/Lifecycle
   (start [component]
     component)
